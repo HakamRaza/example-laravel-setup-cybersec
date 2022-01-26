8e5991ee8b12bf800dcd17cb872ab1e7777e6fcc
HakamRaza <ismatcd@yahoo.com>
2022-01-26 20:37:29 +0800

    fix: forcing use of secure https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         *
+         * Force all connection to use https instead of either http or https
+         * mostly configure at Cloudflare or your server. So just leave here as note
+         *
+         */
+        // if($this->app->environment('production')) {
+        //     \URL::forceScheme('https');
+        // }
     }
 }

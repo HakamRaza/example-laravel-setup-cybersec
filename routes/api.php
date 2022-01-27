d7206f37b6d30e03e1f7bf5b0bee69eba4e566f0
HakamRaza <ismatcd@yahoo.com>
2022-01-27 09:31:58 +0800

    fix: retrieving env key

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
@@ -26,3 +27,25 @@ Route::get('/test-log', function (Request $request) {
 
     return $request->user();
 });
+
+
+Route::get('/test-key', function (Request $request) {
+
+    /**
+     * Retrieve key from env this way
+     */
+    $someKey = config("services.testservice.key");
+    $someTestKey = config("services.testservice.test_key");
+
+    /**
+     * NOT this way
+     */
+    // $someKey = env("IMPORTANT_KEY");
+    // $someTestKey = env("TEST_IMPORTANT_KEY");
+
+    return "This is key obtained :".$someKey." ".$someTestKey;
+});
+
+
+
+Route::middleware('auth:sanctum')->apiResource('/user', UserController::class);

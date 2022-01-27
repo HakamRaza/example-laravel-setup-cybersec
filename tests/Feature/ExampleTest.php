cfd6124aa0df85cd2164d29a82f7b1008ac5b937
HakamRaza <ismatcd@yahoo.com>
2022-01-27 10:06:04 +0800

    tests: add some test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,54 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
-     * @return void
+     * How to add logs
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    public function test_add_some_log(){
+        $this->json('GET', 'api/test-log')
+        ->assertOk()
+        ->assertJson([
+            "message" => "log created, go to storage\logs to see the logs"
+        ]);
+    }
 
-        $response->assertStatus(200);
+    /**
+     * How to obtain key from ENV
+     */
+    public function test_obtain_key_from_env(){
+        $this->json('GET', 'api/test-key')
+        ->assertOk()
+        ->assertJson([
+            "message" => "This is key obtained :1234567890123456789qwerty "
+        ]);
+    }
+
+
+
+
+
+
+    /**
+     * Attacker attempt to change salary value to RM 10
+     * Prevent mass assignment
+     *
+     */
+    public function test_mass_assignable_salary_fail()
+    {
+        $this->json('POST', 'api/mass-assignable', [
+            "user_id" => 1,
+            "salary" => "10"
+        ])
+        ->assertOk()
+        ->assertJson([
+            "user_id" => 1,
+            "salary" => "4000",
+        ]);
     }
 }
